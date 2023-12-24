56bc8b2d0edfc70cb862b0af340c2fea6f69faf8
Ho Quang Minh <minhtc11b4@gmail.com>
2023-12-24 18:32:37 +0700

    first commit

--- src/ComplexityExercise.java
+++ src/ComplexityExercise.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComplexityExercise {
     //O(n.log n)
     static void exercise1( int n ) {
@@ -44,18 +47,55 @@ public class ComplexityExercise {
             }
         }
     }
-    //O(n^2\)
-    static void exercise6( int n ) {
+
+    //O(n^2)
+    static void exercise6(int n) {
         int product = 1;
-        for ( int i = 1; i <= n; i++ ) {
-            for ( int j = 1; j <= i; j++ ) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 product *= 2;
             }
         }
     }
 
-    public static void main(String[] args) {
-        ComplexityExercise complexityExercise = new ComplexityExercise();
-        complexityExercise.exercise1(5);
+    // O(2^n)
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    // O(n!)
+    static List<List<Integer>> generatePermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generate(nums.length, nums, result);
+        return result;
+    }
+
+    static void generate(int n, int[] nums, List<List<Integer>> result) {
+        if (n == 1) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            result.add(permutation);
+        } else {
+            for (int i = 0; i < n - 1; i++) {
+                generate(n - 1, nums, result);
+                if (n % 2 == 0) {
+                    swap(nums, i, n - 1);
+                } else {
+                    swap(nums, 0, n - 1);
+                }
+            }
+            generate(n - 1, nums, result);
+        }
+    }
+
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
